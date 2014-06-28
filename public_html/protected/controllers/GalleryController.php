45fd10aad7af4389d6a72e928e2da021fecd053a
RangeWeb <osadrum@gmail.com>
2014-06-29 00:45:23 +0400

    Обновил

---
+++ public_html/protected/controllers/GalleryController.php
@@ -0,0 +1,19 @@
+<?php
+
+class GalleryController extends Controller
+{
+  /*  public function actionAlbum()
+    {
+        $criteria = new CDbCriteria();
+        $criteria->condition = 'is_published=1';
+
+
+        $dataProvider=new CActiveDataProvider('GalleryCategory', array(
+            'criteria'=>$criteria,
+            'pagination'=>array(
+                'pageSize'=>15,
+            ),
+        ));
+        $this->render('index',array('catalog'=>$dataProvider));
+    }*/
+}

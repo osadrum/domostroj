adc77a9bc92643bced088be809d820ba1d843158
range-web <admin@rangeweb.ru>
2014-06-30 16:22:57 +0400

    шаблон

--- public_html/themes/domostroj/views/layouts/home.php
+++ public_html/themes/domostroj/views/layouts/home.php
@@ -154,56 +154,34 @@
     <footer class="footer">
         <div class="container">
             <div class="row">
+
                 <div class="col-md-3">
                     <div class="col">
-                        <h4>Contact us</h4>
+                        <h2>Решено! Я хочу дом.</h2>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="col text-center menu-bottom">
+                            <?php $cnt=0 ?>
+                            <?php foreach ($this->menuItems as $item) : ?>
+                                <?php
+                                $cnt++;
+                                if ($cnt > 1) {
+                                    echo ' | ';
+                                }
+                                ?>
+                                <?php echo CHtml::link($item['label'], Yii::app()->createUrl($item['url'][0])) ?>
+                            <?php endforeach; ?>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="col">
+                        <h4>Контактная информация</h4>
                         <ul>
-                            <li>5th Avenue, New York - United States</li>
-                            <li>Phone: +10 724 1234 567 | Fax: +10 724 1234 567 </li>
-                            <li>Email: <a href="mailto:hello@example.com" title="Email Us">hello@example.com</a></li>
-                            <li>Skype: <a href="skype:my.business?call" title="Skype us">my-business</a></li>
-                            <li>Creating great templates is our passion</li>
+                            <li><?php echo Settings::getCacheValue('address'); ?></li>
+                            <li>Тел.: <?php echo Settings::getCacheValue('phone'); ?></li>
+                            <li>Email: <?php echo Settings::getCacheValue('email'); ?></li>
                         </ul>
-                    </div>
-                </div>
-
-                <div class="col-md-3">
-                    <div class="col">
-                        <h4>Mailing list</h4>
-                        <p>Sign up if you would like to receive occasional treats from us.</p>
-                        <form class="form-inline">
-                            <div class="input-group">
-                                <input type="text" class="form-control" placeholder="Your email address...">
-                            <span class="input-group-btn">
-                                <button class="btn btn-two" type="button">Go!</button>
-                            </span>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
-                <div class="col-md-3">
-                    <div class="col col-social-icons">
-                        <h4>Follow us</h4>
-                        <a href="#"><i class="fa fa-facebook"></i></a>
-                        <a href="#"><i class="fa fa-google-plus"></i></a>
-                        <a href="#"><i class="fa fa-linkedin"></i></a>
-                        <a href="#"><i class="fa fa-twitter"></i></a>
-                        <a href="#"><i class="fa fa-skype"></i></a>
-                        <a href="#"><i class="fa fa-pinterest"></i></a>
-                        <a href="#"><i class="fa fa-youtube-play"></i></a>
-                        <a href="#"><i class="fa fa-flickr"></i></a>
-                    </div>
-                </div>
-
-                <div class="col-md-3">
-                    <div class="col">
-                        <h4>About us</h4>
-                        <p>
-                            Boomerang Bootstrap Template is made with love and passion for your own business.
-                            <br><br>
-                            <a href="#" class="btn btn-two">Try it now!</a>
-                        </p>
                     </div>
                 </div>
             </div>
@@ -212,12 +190,10 @@
 
             <div class="row">
                 <div class="col-lg-9 copyright">
-                    2013 © Web Pixels. All rights reserverd.
-                    <a href="#">Terms of use</a> |
-                    <a href="#">Privacy policy</a>
+                    2014 <?php echo (date('Y') > 2014)?' - '.date('Y'):''; ?> © <?php echo Settings::getCacheValue('siteName'); ?>. Все права защиещены.
                 </div>
                 <div class="col-lg-3 footer-logo">
-
+                    <a href="http://rangeweb.ru" title="Разработка сайта"><img src="http://rangeweb.ru/shared/logo/logo130x21.png" alt="Разработка сайта"></a>
                 </div>
             </div>
         </div>

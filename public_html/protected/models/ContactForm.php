4e43b30a9b16ffc288825d2770f387a708b02c6c
range-web <admin@rangeweb.ru>
2014-07-01 12:26:25 +0400

    контакты

--- public_html/protected/models/ContactForm.php
+++ public_html/protected/models/ContactForm.php
@@ -41,18 +41,8 @@ class ContactForm extends CFormModel
             'subject'=>'Тема сообщения',
             'body'=>'Текст сообщения',
             'phone'=>'Телефон',
+            'verifyCode'=>'Verification Code',
         );
     }
 
-	/**
-	 * Declares customized attribute labels.
-	 * If not declared here, an attribute would have a label that is
-	 * the same as its name with the first letter in upper case.
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'verifyCode'=>'Verification Code',
-		);
-	}
 }
